3fc652ce709e163b4fd5b7f1ec67c6c49c528daf
JGilbe28-7 <jonathan.gilbert@mac.com>
2020-10-31 10:32:55 -0400

    updated the script.js youtube API

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-//
-
 // buttons & onlick events
 // search card interactions (buttons etc)
 // making the modal appear with the results
 // shows the content when activated 
+//youtube code
+function getVideo() {
+    var test = $("#search-term").val()
+    console.log(test)
+  
+    $.ajax({
+        type: 'GET',
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        data: {
+            key: 'AIzaSyCBVJVvBCExc4LG0QasZwIOwB8Tjh0sCYk',
+            q: test, //need to link to search box
+            part: 'snippet',
+            channelId: 'UC4a-Gbdw7vOaccHmFo40b9g',
+            maxResults: 1,
+            type: 'video',
+            videoEmbeddable: true,
+        },
+        success: function(data){
+            embedVideo(data)
+        },
+        error: function(response){
+            console.log("Request Failed");
+        }
+      });
+    }
+  
+    function embedVideo(data) {
+      $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
+      $('h3').text(data.items[0].snippet.title)
+      $('.description').text(data.items[0].snippet.description)
+  }
+  
