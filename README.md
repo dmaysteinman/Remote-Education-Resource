2b9cead0047525b4031a9df2fbb5b0874928cb68
dmaysteinman <69429439+dmaysteinman@users.noreply.github.com>
2020-11-03 18:44:50 -0500

    Ed data api logic (#13)
    * Retrieve ed data based on user inputs
    
    * Update README
    
    * Update README
    
    Co-authored-by: JES20 <dore.joel.dore@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Remote Education Resource
 
-## _A single-page website for parents facing the challenges of remote education_
+### _A single-page website for parents facing the challenges of remote education_
 
 ![Demo](https://www.placecage.com/600/300)  
 Currently deployed at [GitHub Pages](https://dmaysteinman.github.io/project_1/)
@@ -15,14 +15,12 @@ Currently deployed at [GitHub Pages](https://dmaysteinman.github.io/project_1/)
 
 ---
 
-## Project Collaborators
-
-<br>
+## Project Collaborators:
 
 [![](https://github.com/dmaysteinman.png?size=64)](https://github.com/dmaysteinman)[![linkedin](Images/linkedin.svg)](http://www.linkedin.com/in/danielle-may-steinman-2868781b4)
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-[<img src="https://media-exp1.licdn.com/dms/image/C4D03AQHTbPrJQ5US1w/profile-displayphoto-shrink_400_400/0?e=1609372800&v=beta&t=UEVpFtEsO78Zr9T9GlUdGmfh9jhzryxTeuREVVkB8x4" width="64">](https://github.com/mrjacoughlin)[![linkedin](Images/linkedin.svg)](http://www.linkedin.com/in/james-coughlin-428a1747)
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+[<img src="https://media-exp1.licdn.com/dms/image/C4D03AQHTbPrJQ5US1w/profile-displayphoto-shrink_400_400/0?e=1609372800&v=beta&t=UEVpFtEsO78Zr9T9GlUdGmfh9jhzryxTeuREVVkB8x4" width="64">](https://github.com/mrjacoughlin)[![linkedin](Images/linkedin.svg)](http://www.linkedin.com/in/james-coughlin-428a1747)  
+
 [![](https://github.com/JGilb28-7.png?size=64)](https://github.com/JGilb28-7)[![linkedin](Images/linkedin.svg)](http://www.linkedin.com/in/jonathan-gilbert-67600211)
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 [![](https://github.com/joeldore.png?size=64)](https://github.com/joeldore)
@@ -40,7 +38,7 @@ Currently deployed at [GitHub Pages](https://dmaysteinman.github.io/project_1/)
 
 - Integrate calendar feature for user to access assignments/due dates
 
-- Expand to accommodate higher levels of schooling
+- Option for user to narrow results by school level/grades offered
 
 ---
 
@@ -50,7 +48,7 @@ Currently deployed at [GitHub Pages](https://dmaysteinman.github.io/project_1/)
 <img src='https://img.shields.io/github/last-commit/dmaysteinman/project_1'>
 <img src='https://img.shields.io/github/languages/top/dmaysteinman/project_1'>
 
-This project is [MIT](https://github.com/JoelDore/REPO-NAME/blob/main/LICENSE) licensed  
+This project is [MIT](https://github.com/dmaysteinman/project_1/blob/main/LICENSE) licensed  
 © 2020 _StopBooleanMe_
 
 </div>
--- logic.js
+++ logic.js
@@ -1,57 +1,41 @@
 function getData() {
 
-const stateSelect = $("#state-select option:selected").text();
-console.log(stateSelect)
+    const stateSelect = $("#state-select option:selected").text();
+    const zipInput = $("#searchBtn-zip").val();
+    const resultsDiv = $("#data-results")
 
-var queryURL =
-  "https://educationdata.urban.org/api/v1/schools/ccd/directory/2018/?state_location=" + stateSelect
+    var queryURL =
+        `https://educationdata.urban.org/api/v1/schools/ccd/directory/2018/?state_location=${stateSelect}`
 
 
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
 
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (response) {
-    console.log(response.results);
+        // Clear resultsDiv
+        resultsDiv.html('')
 
-    let results=response.results;
-  
-    
-    for (const i = 0; i<=results.length; i++) {
-      const schoolName = results[i].school_name;
-      console.log(schoolName);
+        let results = response.results;
 
-      const virtual = results[i].virtual;
-        if (virtual === 0) {
-        console.log("no");
-      } else if (virtual === 1) {
-        console.log("yes");
-      } else {
-        console.log("not sure")
-      }
+        for (let i = 0; i < results.length; i++) {
 
-      const state = response.results[i].state_location;
-        if (state === "VA") {
-          console.log("Virginia")
-        } else if (state === "NH") {
-          console.log("New Hampshire")
-        } else if (state === "ME") {
-            console.log("Maine")
-        } else {
-          console.log("Other")
+            const zipLocation = results[i].zip_location;
+            const zipMailing = results[i].zip_mailing;
+
+            if (zipInput === zipLocation || zipInput === zipMailing) {
+                const schoolName = results[i].school_name;
+                const phoneNumber = results[i].phone;
+                const enrollment = results[i].enrollment;
+
+                // Populate Results text with data
+                resultsDiv.append(`School Name: ${schoolName} <br>Phone: ${phoneNumber} <br>Enrollment: ${enrollment} students <br><br>`)
+
+                // For reference
+                console.log(schoolName)
+                console.log(phoneNumber)
+                console.log(enrollment)
+            }
         }
-
-        // The zip code for loop etc
-      // const zip = response.results[i].zip_location;
-      //   if (zip === "") {
-      //     console.log(schoolName)
-      //   }
-      
-          
-          
-      }
-
-
-  
-});
+    });
 }
