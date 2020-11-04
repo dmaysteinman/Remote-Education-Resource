b8f9adb4b36263ae81111a85c3504fa05cadced9
JES20 <dore.joel.dore@gmail.com>
2020-11-03 20:50:37 -0500

    Update API key, search dropdown for youtube channel

--- logic.js
+++ logic.js
@@ -1,3 +1,12 @@
+const statesArray = ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'];
+
+$(document).ready(() => {
+    const stateSelectElement = $("#state-select");
+    statesArray.forEach((item) => {
+        stateSelectElement.append(`<option>${item}</option>`);
+    })
+})
+
 function getData() {
 
     const stateSelect = $("#state-select option:selected").text();
@@ -28,8 +37,8 @@ function getData() {
                 const phoneNumber = results[i].phone;
                 const enrollment = results[i].enrollment;
 
-                // Populate Results text with data
-                resultsDiv.append(`School Name: ${schoolName} <br>Phone: ${phoneNumber} <br>Enrollment: ${enrollment} students <br><br>`)
+                // APPEND data to results div
+                resultsDiv.append(`<div class='card m-2 bg-light'><div class='card-body'><b>School Name:</b> ${schoolName} <br> <b>Phone:</b> ${phoneNumber} <br> <b>Enrollment:</b> ${enrollment} students</div></div>`)
 
                 // For reference
                 console.log(schoolName)
