cbbeb6f967eb168c54e114858dfbc5ddeb16abd8
JES20 <dore.joel.dore@gmail.com>
2020-11-05 12:14:36 -0500

    Update script.js - results hidden initially, appended on search

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-// buttons & onlick events
-// search card interactions (buttons etc)
-// making the modal appear with the results
-// shows the content when activated 
-//youtube code
-let channelIds = {
+// Logic for YouTube API Call
+
+const channelIds = {
     khanAcademy: ["UC4a-Gbdw7vOaccHmFo40b9g", "Khan Academy"], sesameStreet:
         ["UCoookXUzPciGrEZEXmh4Jjg", "Sesame Street"], smithsonianChannel: ["UCWqPRUsJlZaDp-PVbqEch9g", "The Smithsonian"], novaPbsOfficial:
         ["UCjHz5SVHeMT0AViCYZvsGDA", "Nova PBS"], crashCourse: ["UCX6b17PVsYBQ0ip5gyeme-Q", "Crash Course"], itsOkToBeSmart: ["UCH4BNI0-FOK2dMXoFtViWHw", "It's Okay To Be Smart"]
 }
+const channelIdsArray = Object.keys(channelIds);
+const vidResultsDiv = $("#video-results");
+const channelSelectElement = $("#channelSelect");
 
 $(document).ready(() => {
-    const channelSelectElement = $("#channelSelect");
-    const channelIdsArray = Object.keys(channelIds);
-
     for (let i = 0; i < channelIdsArray.length; i++) {
         channelSelectElement.append(`<option value="${channelIds[channelIdsArray[i]][0]}">${channelIds[channelIdsArray[i]][1]}</option>`);
     }
@@ -21,15 +18,12 @@ $(document).ready(() => {
 function getVideo() {
     const channelIdSelect = $("#channelSelect option:selected").val();
     var searchTerm = $("#search-term").val()
-    console.log(searchTerm)
-    console.log()
     $.ajax({
         type: 'GET',
         url: 'https://www.googleapis.com/youtube/v3/search',
         data: {
-            // Jon's key: 'AIzaSyCBVJVvBCExc4LG0QasZwIOwB8Tjh0sCYk'
-            key: 'AIzaSyAHi1lxmJnNf9lk23FC0XZPOKihGFIlFqk',
-            q: searchTerm, //need to link to search box
+            key: 'AIzaSyAHi1lxmJnNf9lk23FC0XZPOKihGFIlFqk', // Joel's key
+            q: searchTerm,
             part: 'snippet',
             channelId: channelIdSelect,
             maxResults: 1,
@@ -38,15 +32,16 @@ function getVideo() {
         },
         success: function (data) {
             embedVideo(data)
-        },
-        error: function (response) {
-            console.log("Request Failed");
         }
     });
 }
 
 function embedVideo(data) {
-    $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
-    $('h3').text(data.items[0].snippet.title)
-    $('.description').text(data.items[0].snippet.description)
+    vidResultsDiv.html('')
+
+    const newIframe = $('<iframe>').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
+    const newTitle = $('<h3>').text(data.items[0].snippet.title)
+    const newDescription = $('<p>').text(data.items[0].snippet.description)
+
+    vidResultsDiv.append(newIframe, newTitle, newDescription);
 }
